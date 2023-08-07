c196f0604c99a817b53f227287ca12ce744c5190
nudara2000 <nudaradilaksha2000@gmail.com>
2023-08-07 22:39:59 +0530

    new changes

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -1,13 +1,12 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView,TouchableOpacity } from "react-native";
 import React from "react";
 import LottieView from "lottie-react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const OrderScreen = () => {
   const navigation = useNavigation();
-  const handleThumbsUpClick = () => {
-    // Function to handle thumbs-up click
-    navigation.navigate("HomeScreen"); // Navigate to the "Home" screen
+  const handleGoBackToHome = () => {
+    navigation.navigate("Home"); // Navigate back to the "Home" screen
   };
 
   return (
@@ -51,6 +50,12 @@ const OrderScreen = () => {
         loop={false}
         speed={0.7}
       />
+      <TouchableOpacity
+          style={styles.button}
+          onPress={handleGoBackToHome}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
     </SafeAreaView>
     </View>
   );
@@ -61,6 +66,23 @@ export default OrderScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#CCCCFF", // Replace this with your desired background color
+    backgroundColor: "#CCCCFF",
+    justifyContent: "flex-end", // Align content to the bottom of the container
+  },
+  button: {
+    backgroundColor: "#34495E",
+    padding: 10,
+    margin: 300,
+    marginBottom: 40, // Add some bottom margin to separate the button from the screen edge
+    marginRight: 20, // Add some right margin to separate the button from the screen edge
+    alignSelf: "flex-end", // Align the button to the right of the container
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
+
+
